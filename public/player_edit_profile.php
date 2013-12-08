3d346192088bf3fc07084d184ccee0cbb23c2b8e
lingzhang <zhangling.ice@gmail.com>
2013-12-07 20:46:08 -0800

    add player_edit_profile.php from Bingyu Li

---
+++ public/player_edit_profile.php
@@ -0,0 +1,90 @@
+<?php require_once("../includes/session.php"); ?>
+<?php require_once("../includes/db_connection.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
+<?php require_once("../includes/validation_functions.php"); ?>
+<?php confirm_player_logged_in(); 
+	$player=find_player_by_id ($_SESSION["player_id"]);
+?>
+<?php
+
+if (isset($_POST['submit'])) {
+  // Process the form
+  
+  // validations
+  //$required_fields = array("user_name","user_email","password");
+  $required_fields = array("player_name","password");
+
+  validate_presences($required_fields);
+  
+  $fields_with_max_lengths = array("player_name" =>20);
+  validate_max_lengths($fields_with_max_lengths);
+  
+  if (empty($errors)) {
+    
+    // Perform Update
+
+    $id = $player["id"];
+    $player_name = mysql_prep($_POST["player_name"]);
+    $player_hashed_password = password_encrypt($_POST["password"]);
+
+    $query  = "UPDATE players SET ";
+    $query .= "username = '{$player_name}', ";
+    $query .= "hashed_password = '{$player_hashed_password}' ";
+    $query .= "WHERE id = {$id} ";
+    $query .= "LIMIT 1";
+    $result = mysqli_query($connection, $query);
+
+    if ($result 
+		//&& mysqli_affected_rows($connection) == 1
+		) {
+      // Success
+      $_SESSION["message"] = "Player update successful.";
+      redirect_to("player.php");
+    } else {
+      // Failure
+      $_SESSION["message"] = "Player update failed.";
+    }
+  
+  }
+} else {
+  // This is probably a GET request
+  
+} // end: if (isset($_POST['submit']))
+?>
+
+
+<?php include("../includes/layouts/header.php"); ?>
+<div class="hlinks">
+   <a href="player.php"><span class="text">Back</span></a>
+</div>
+</div>
+
+<div id="main">
+  <div id="navigation">
+    &nbsp;
+  </div>
+
+  <div id="page">
+    <?php echo message(); ?>
+    <?php echo form_errors($errors); ?>
+    
+    <h2>Edit Your Profile: <?php echo htmlentities($player["username"]); ?></h2>
+    <form action="player_edit_profile.php?id=<?php echo urlencode($_SESSION['player_id']); ?>" method="post">
+
+		<p>Player Name<br></br>
+		<input type="text" name="player_name" value="<?php echo htmlentities($player['username']); ?>" />
+		</p>		
+
+		<p>Password<br></br>
+		<input type="password" name="password" value="" />
+		</p>
+		<p>Membership<br></br>
+		<?php echo htmlentities($player["membership"]); ?>
+		</p>
+		
+		<input type="submit" name="submit" value="Edit Player" class="blue" />
+    </form>
+    <br />
+  </div>
+</div>
+<?php include("../includes/layouts/footer.php"); ?>
